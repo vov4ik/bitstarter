d7abfc3aec91f1689f5bfb31bba8eff3aab6184c
Ubuntu <ubuntu@ip-172-31-38-117.us-west-2.compute.internal>
2013-07-10 21:38:57 +0000

    Responding with index.html

--- web.js
+++ web.js
@@ -1,12 +1,21 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! 2');
+  var name = 'index.html';
+  fs.readFile(name, function (err, html) {
+    if(err) {
+    	console.log(err);
+    } else {
+    	response.send(html);
+    	console.log('Sending ' + name);
+    }
+  });
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log('Listening on ' + port);
 });
